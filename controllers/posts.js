87740753be2b988fc26dce79ff2d335fb1d5c82b
Zach Eggert <egger0a6@email.arizona.edu>
2022-07-15 14:13:43 -0400

    add create function to post controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,20 @@
 import { Post } from "../models/post.js"
 
 function create(req, res) {
-  console.log(req)
+  console.log(req.body)
+  req.body.owner = req.user.profile
+  Post.create(req.body)
+  .then(post => {
+    Post.findById(post._id)
+    .populate("owner")
+    .then(populatedPost => {
+      res.json(populatedPost)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
 }
 
 function index(req, res) {
