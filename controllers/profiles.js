782f7bf5669339832b2f4d2db7a8b6eae45934a3
Jamie Ahmed <jamieahmed273@gmail.com>
2022-07-18 11:14:58 -0400

    add show functionality in backend for account

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -2,11 +2,22 @@ import { Profile } from '../models/profile.js'
 
 function index(req, res) {
   Profile.find({})
-  .then(profiles => res.json(profiles))
-  .catch(err => {
-    console.log(err)
-    res.status(500).json(err)
-  })
+    .then(profiles => res.json(profiles))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 }
 
-export { index }
+function show(req, res) {
+  Profile.findById(req.params.id)
+    .then((profile) => {
+      res.json(profile)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ err: err.errmsg })
+    })
+}
+
+export { index, show }
