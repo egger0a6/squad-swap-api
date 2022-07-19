e11c9ef59726ff3d875dbf6a72509d80ab400c69
Zach Eggert <egger0a6@email.arizona.edu>
2022-07-19 01:38:31 -0400

    write offer controller create function

--- controllers/offers.js
+++ controllers/offers.js
@@ -1,7 +1,19 @@
 import { Offer } from "../models/offer.js"
 
 function create(req, res) {
-  console.log(req.body)
+  req.body.owner = req.user.profile
+  Offer.create(req.body)
+  .then(offer => {
+    Offer.findById(offer._id)
+    .populate("owner")
+    .then(populatedOffer => {
+      res.json(populatedOffer)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
 }
 
 export {
