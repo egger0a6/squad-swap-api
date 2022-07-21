9c010c56593bcd5d72ef5eec053f7b1f2c910990
Zach Eggert <egger0a6@email.arizona.edu>
2022-07-21 01:10:07 -0400

    change the way in which the cloudinary url is being added to instances of the profile and post models in their controller's addPhoto fns: secure_url is needed to display in all web browsers

--- controllers/posts.js
+++ controllers/posts.js
@@ -86,7 +86,8 @@ function addPhoto(req, res) {
   .then(post => {
     cloudinary.uploader.upload(imageFile, {tags: `${post.title}`})
     .then(image => {
-      post.photo = image.url
+      console.log(image)
+      post.photo = image.secure_url
       post.save()
       .then(post => {
         res.status(201).json(post.photo)
